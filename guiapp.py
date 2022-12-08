30bd706c4be84aa8a77cf2352cb147837457e5b0
ritish <sritish.10@gmail.com>
2022-12-08 13:24:24 +0530

    first commit

--- guiapp.py
+++ guiapp.py
@@ -296,7 +296,7 @@ def format_time(secs):
 
 
 def main():
-    win = pygame.display.set_mode((540,600))
+    win = pygame.display.set_mode((600,600))
     pygame.display.set_caption("Sudoku (Press Space to auto solve) ")
     board = Grid(9, 9, 540, 540, win)
     key = None
@@ -312,57 +312,13 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_1:
-                    key = 1
-                if event.key == pygame.K_2:
-                    key = 2
-                if event.key == pygame.K_3:
-                    key = 3
-                if event.key == pygame.K_4:
-                    key = 4
-                if event.key == pygame.K_5:
-                    key = 5
-                if event.key == pygame.K_6:
-                    key = 6
-                if event.key == pygame.K_7:
-                    key = 7
-                if event.key == pygame.K_8:
-                    key = 8
-                if event.key == pygame.K_9:
-                    key = 9
-                if event.key == pygame.K_DELETE:
-                    board.clear()
-                    key = None
-
                 if event.key == pygame.K_SPACE:
                     board.solve_gui()
-
-                if event.key == pygame.K_RETURN:
-                    i, j = board.selected
-                    if board.cubes[i][j].temp != 0:
-                        if board.place(board.cubes[i][j].temp):
-                            print("Success")
-                        else:
-                            print("Wrong")
-                            strikes += 1
-                        key = None
-
-                        if board.is_finished():
-                            print("Game over")
-
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                pos = pygame.mouse.get_pos()
-                clicked = board.click(pos)
-                if clicked:
-                    board.select(clicked[0], clicked[1])
-                    key = None
-
         if board.selected and key != None:
             board.sketch(key)
             
         redraw_window(win, board, play_time, strikes)
         pygame.display.update()
-
 
 main()
 pygame.quit()
